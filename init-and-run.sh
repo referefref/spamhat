2d88d7c7c70e13c4a47572b70683ccc042e60c24
ref <56499429+referefref@users.noreply.github.com>
2024-05-15 12:41:56 +0800

    Update init-and-run.sh

--- init-and-run.sh
+++ init-and-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Wait for MariaDB to be ready
-until mysql -h 127.0.0.1 -u spamhat -p password -e "SELECT 1" &> /dev/null; do
+until mysql -h mariadb -u spamhat -p password -e "SELECT 1" &> /dev/null; do
   echo "Waiting for MariaDB..."
   sleep 5
 done
