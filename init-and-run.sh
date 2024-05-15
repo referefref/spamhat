e5f3298d29800f3cfd01add2e05b3906d6bdac7d
ref <56499429+referefref@users.noreply.github.com>
2024-05-15 11:48:26 +0800

    Update init-and-run.sh

--- init-and-run.sh
+++ init-and-run.sh
@@ -1,18 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Wait for MariaDB to be ready
 until mysql -hmariadb -uspamhat -ppassword -e "SELECT 1" &> /dev/null; do
   echo "Waiting for MariaDB..."
   sleep 5
 done
-
-# Initialise the database
-mysql -hmariadb -uroot -proot_password <<EOF
-CREATE DATABASE IF NOT EXISTS spamhat;
-CREATE USER IF NOT EXISTS 'spamhat'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON spamhat.* TO 'spamhat'@'%';
-FLUSH PRIVILEGES;
-EOF
 
 # Execute the main script
 exec /root/spamhat/runpot.sh
